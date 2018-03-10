e4f35abf49d9bc66357864f338420e9c4cef7c93
shruthijoe <37235137+shruthijoe@users.noreply.github.com>
2018-03-10 01:37:54 -0800

    Update positive.py

--- positive.py
+++ positive.py
@@ -1,14 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      RGCET
-#
-# Created:     10/03/2018
-# Copyright:   (c) RGCET 2018
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 a = float(input("Enter a number: "))
 if (a>0) :
    print("Positive number")
