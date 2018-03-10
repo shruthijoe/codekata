cc6656a0d66e0e4d468c78839fbf37294cbe5bd0
shruthijoe <37235137+shruthijoe@users.noreply.github.com>
2018-03-10 01:36:04 -0800

    Add files via upload

---
+++ odd.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      RGCET
+#
+# Created:     09/03/2018
+# Copyright:   (c) RGCET 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+k = int(input("Enter a number: "))
+if (k % 2) == 0:
+   print("{0} is Even".format(k))
+else:
+   print("{0} is Odd".format(k))
---
+++ positive.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      RGCET
+#
+# Created:     10/03/2018
+# Copyright:   (c) RGCET 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+a = float(input("Enter a number: "))
+if (a>0) :
+   print("Positive number")
+elif (a<0):
+   print("negative number")
+else:
+   print("Zero")
